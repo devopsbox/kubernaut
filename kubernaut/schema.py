f334441e98fdae5733f4092549e5bd2205dbcb75
Philip Lombardi <plombardi@datawire.io>
2017-11-02 18:48:30 -0400

    Remove the JSON schema stuff for the kubernaut.yaml file (for now)

--- kubernaut/schema.py
+++
@@ -1,7 +0,0 @@
-import json
-import pkg_resources
-
-
-def load_schema(name):
-    schema = pkg_resources.resource_string(__name__, "/".join(("jsonschema", name)))
-    return json.loads(schema.decode("UTF-8"))
