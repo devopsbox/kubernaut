8bde90bddad9b9bd4dfe0ff1f53ee8114e94c8cd
Philip Lombardi <plombardi@datawire.io>
2017-11-14 11:42:31 -0500

    Fix module import

--- kubernaut/auth/commands.py
+++ kubernaut/auth/commands.py
@@ -1,16 +1,14 @@
 import click
 
+from kubernaut.messages import GET_TOKEN
+
 
 @click.command(
     name="get-token",
     help="Get an authentication token"
 )
-def cli_get_token():
-    msg = click.style("Kubernaut is a free service! Please get an access token to use Kubernaut\n\n")
-    msg += click.style("https://kubernaut.io/token", bold=True, underline=True)
-    msg += click.style("\n\nAfterwards run `kubernaut set-token <TOKEN>`")
-
-    click.echo(msg)
+def cli_get_token() -> None:
+    click.echo(GET_TOKEN)
 
 
 @click.command(
@@ -19,5 +17,5 @@ def cli_get_token():
 )
 @click.argument("token")
 @click.pass_obj
-def cli_set_token(kubernaut, token):
+def cli_set_token(kubernaut, token) -> None:
     kubernaut.update_config("token", token)
